a2a5d5d6e147e8ca8b62418b890ea13b8d8f1665
Anders Austad <anders.austad@novanet.no>
2018-01-14 18:41:38 +0100

    add azure function skeleton

---
+++ StrevdeAzureFunctions/StrevdeAzureFunctions/Models/ActivitiesRequestBody.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace StrevdeAzureFunctions.Models
+{
+    public class ActivitiesRequestBody
+    {
+        public IEnumerable<string> ActivityIDs { get; set; }
+    }
+}
