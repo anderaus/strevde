cecafd725c64d337372afad06d935ff2f81cda71
Anders Austad <anders.austad@novanet.no>
2018-01-16 19:35:46 +0100

    Refactor

---
+++ StrevdeAzureFunctions/StrevdeAzureFunctions/Services/StravaFetcher.cs
@@ -0,0 +1,36 @@
+﻿using StrevdeAzureFunctions.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace StrevdeAzureFunctions.Services
+{
+    public class StravaFetcher : IActivityFetcher
+    {
+        private readonly HttpClient _httpClient;
+
+        public StravaFetcher(HttpClient httpClient, string stravaBearerToken)
+        {
+            _httpClient = httpClient;
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", stravaBearerToken);
+        }
+
+        public async Task<IEnumerable<StravaActivity>> FetchActivities(IEnumerable<string> activityIDs)
+        {
+            var activities = new List<StravaActivity>();
+
+            foreach (var activityId in activityIDs)
+            {
+                var response = await _httpClient.GetAsync(new Uri($"https://www.strava.com/api/v3/activities/{activityId}"));
+                response.EnsureSuccessStatusCode();
+
+                var activity = await response.Content.ReadAsAsync<StravaActivity>();
+                activities.Add(activity);
+            }
+
+            return activities;
+        }
+    }
+}
