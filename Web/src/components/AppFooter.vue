b136e9d30f0eddd5b3c4d08a39d11e000fdee4da
Anders Austad <anderaus@gmail.com>
2018-01-24 20:11:03 +0100

    Add hardcoded header & footer

---
+++ Web/src/components/AppFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+          <p>
+            <strong>Strevde</strong> is a rainy Sunday hack by
+            <a href="http://www.twitter.com/anderaus">Anders Austad</a>
+          </p>
+          <p>
+            <img class="strava-logo" src="../assets/api_logo_pwrdBy_strava_stack_light.svg">
+          </p>
+        </div>
+      </div>
+    </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.strava-logo {
+  height: 3rem;
+}
+</style>
---
+++ Web/src/components/AppHeader.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="hero is-primary is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-half">
+            <h1 class="title is-3">
+              {{title}}
+            </h1>
+            <p class="subtitle">
+              {{subtitle}}
+            </p>
+          </div>
+          <div class="column is-half">
+            <div class="level is-mobile">
+              <div class="level-item has-text-centered">
+                <div>
+                  <p class="title">{{activities ? activities.length : 0}}</p>
+                  <p class="heading">Days</p>
+                </div>
+              </div>
+              <div class="level-item has-text-centered">
+                <div>
+                  <p class="title">{{total_distance / 1000 | round(1)}}</p>
+                  <p class="heading">Kilometers</p>
+                </div>
+              </div>
+              <div class="level-item has-text-centered">
+                <div>
+                  <p class="title">{{total_elevation | round(0)}}</p>
+                  <p class="heading">Meters climb</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="map"></div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hardcoded title',
+      subtitle: 'And a hardcoded subtitle as well',
+      total_distance: 12345.67,
+      total_elevation: 876,
+      activities: [0, 1, 2]
+    };
+  }
+};
+</script>
