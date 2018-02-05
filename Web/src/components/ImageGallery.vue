69684c51714e04a9d5795b97046b2f18dd10c3d5
Anders Austad <anders.austad@novanet.no>
2018-02-05 19:17:21 +0100

    Simplify css

--- Web/src/components/ImageGallery.vue
+++ Web/src/components/ImageGallery.vue
@@ -1,5 +1,5 @@
 <template>
-   <section class="section is-clearfix gallery">
+   <section class="section is-clearfix">
         <div class="container">
             <h2 class="title">Photos</h2>
             <hr>
@@ -25,7 +25,7 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.gallery .image img {
+.image img {
   object-fit: cover;
 }
 </style>
