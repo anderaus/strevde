d54d21b86c17042997bb3b738a7f475f4b6fee87
Anders Austad <anderaus@gmail.com>
2018-02-05 17:50:16 +0100

    Add trip details component

--- Web/src/main.js
+++ Web/src/main.js
@@ -4,9 +4,11 @@ import App from './App'
 import store from './store'
 
 import { round } from './filters/round.js'
+import { friendlytime } from './filters/friendlytime.js'
 
 // register filters globally
 Vue.filter('round', round)
+Vue.filter('friendlytime', friendlytime)
 
 document.addEventListener("DOMContentLoaded", function (event) {
   new Vue({
