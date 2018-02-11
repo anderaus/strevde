e316c439db3a16a121240721462cb35a08f5ad63
Anders Austad <anders.austad@novanet.no>
2018-02-11 17:27:54 +0100

    Add photoswipe basics

--- Web/src/components/ImageGallery.vue
+++ Web/src/components/ImageGallery.vue
@@ -1,31 +1,51 @@
 <template>
    <section class="section is-clearfix">
-        <div class="container">
-            <h2 class="title">Photos</h2>
-            <hr>
-            <div class="columns is-mobile is-multiline">
-                <div class="column is-one-third-mobile is-3-tablet is-2-desktop" v-for="photo in photos" :key="photo.id">
-                    <figure class="image is-square" >
-                        <img :src="photo.url" />
-                    </figure>
-                </div>
-            </div>
+      <div class="container">
+        <h2 class="title">Photos</h2>
+        <hr>
+        <div class="columns is-mobile is-multiline">
+          <div class="column is-one-third-mobile is-3-tablet is-2-desktop" v-for="(photo, index) in photos" :key="photo.id">
+            <figure class="image is-square" >
+              <img :src="photo.url" @click="onImageClick(index)" />
+            </figure>
+          </div>
         </div>
+      </div>
+      <gallery-lightbox :photos="lightboxPhotos"></gallery-lightbox>
     </section>
 </template>
 
 <script>
+import GalleryLightbox from './GalleryLightbox';
+import { EventBus } from '../common/event-bus.js';
 import { mapGetters } from 'vuex';
 
 export default {
   name: 'ImageGallery',
+  components: {
+    GalleryLightbox
+  },
+  methods: {
+    onImageClick: function(imageIndex) {
+      EventBus.$emit('imageClick', imageIndex);
+    }
+  },
   computed: {
-    ...mapGetters(['photos'])
+    ...mapGetters(['photos']),
+    lightboxPhotos() {
+      return this.photos.map(p => ({
+        src: p.url,
+        h: p.height,
+        w: p.width
+      }));
+    }
   }
 };
 </script>
+
 <style lang="scss" scoped>
 .image img {
   object-fit: cover;
+  cursor: pointer;
 }
 </style>
