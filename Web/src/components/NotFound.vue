48abe8f69e7e7443681630f110ac375575eee6e1
Anders Austad <anders.austad@novanet.no>
2018-04-07 14:38:30 +0200

    Switch to HTML5 history pushstate mode Add notfound page

---
+++ Web/src/components/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+     <section class="section">
+      <div class="container">
+        <h1 class="title">Oh snap! Page not found 😥</h1>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+};
+</script>
--- Web/src/router/index.js
+++ Web/src/router/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Trip from '@/components/Trip'
 import Login from '@/components/Login'
+import NotFound from '@/components/NotFound'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -21,6 +23,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
     }
   ]
 })
