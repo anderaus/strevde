322dc1eef1280cba9f4f0f8b547492482f480d3a
Anders Austad <anders.austad@novanet.no>
2018-05-01 19:55:38 +0200

    Add auth api skeleton

---
+++ Api/Strevde.API/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Strevde.API.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet("~/signin"), HttpPost("~/signin")]
+        public IActionResult SignIn(string returnUrl)
+        {
+            return Challenge(new AuthenticationProperties { RedirectUri = string.IsNullOrEmpty(returnUrl) ? "/" : returnUrl }, "Strava");
+        }
+
+        [HttpGet("~/signout"), HttpPost("~/signout")]
+        public IActionResult SignOut()
+        {
+            return SignOut(new AuthenticationProperties { RedirectUri = "/" },
+                CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+    }
+}
---
+++ Api/Strevde.API/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Strevde.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/signin";
+                    options.LogoutPath = "/signout";
+                })
+                .AddStrava(options =>
+                {
+                    options.ClientId = Configuration.GetValue<string>("appSettings:stravaClientId");
+                    options.ClientSecret = Configuration.GetValue<string>("appSettings:stravaClientSecret");
+                    options.SaveTokens = true;
+                });
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
